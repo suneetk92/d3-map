2ef0a280952de46a8f0a637be56f420e0edad45f
Suneet Kumar <suneetk92@gmail.com>
2021-05-17 00:42:20 +0530

    remove unused d3 libraries

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,12 @@ import {
   OnInit,
   ViewEncapsulation,
 } from '@angular/core';
-import * as d3 from 'd3';
-import { GeoProjection, Selection } from 'd3';
+import { geoOrthographic, GeoProjection, geoPath } from 'd3-geo';
+import { Selection, select } from 'd3-selection';
 import { MapService } from './map.service';
 import { Feature } from 'geojson';
+import { schemeCategory10 } from 'd3-scale-chromatic';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -23,12 +25,16 @@ export class AppComponent implements OnInit {
   height: number;
   width: number;
 
+  xAngle: number = 285;
+
   visReady: boolean;
   highlighted: string | undefined | null;
   clicked: string | undefined | null;
+  interval!: any;
+  subscribe: Subscription | undefined;
 
   constructor(private el: ElementRef, private mapService: MapService) {
-    this.geoProjection = d3.geoOrthographic();
+    this.geoProjection = geoOrthographic();
     this.visReady = false;
     this.height = 500;
     this.width = 500;
@@ -36,10 +42,14 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.visReady = false;
+    this.initializeGlobe();
+  }
+
+  private initializeGlobe() {
     this.height = this.el.nativeElement.offsetHeight;
     this.width = this.el.nativeElement.offsetWidth;
 
-    this.mapChart = d3.select('#map');
+    this.mapChart = select('#map');
 
     this.mapChart
       .append('circle')
@@ -51,17 +61,13 @@ export class AppComponent implements OnInit {
     const countriesGroup: Selection<SVGGElement, void, HTMLElement, any> =
       this.mapChart.append('g').attr('class', 'map');
 
-    let xAngle = 285;
-
-    let geoPath = d3.geoPath(
+    let path = geoPath(
       this.geoProjection
         .translate([this.width / 2, this.height / 2])
-        .rotate([xAngle, 0])
+        .rotate([this.xAngle, 0])
     );
 
-    this.mapService.fetchCountryGeoJSON().subscribe((data) => {
-      let interval: any;
-
+    this.subscribe = this.mapService.fetchCountryGeoJSON().subscribe((data) => {
       const text: Selection<SVGTextElement, void, HTMLElement, any> =
         this.mapChart
           .append('text')
@@ -86,37 +92,39 @@ export class AppComponent implements OnInit {
           .on('click', (d, data) => {
             if (data.id !== this.clicked) {
               this.clicked = data.id as string;
-              clearInterval(interval);
+              clearInterval(this.interval);
               globe.classed('clicked', (d) => d.id === data.id);
               text.text(data.id as string).attr('dx', '-35px');
             } else {
               this.clicked = null;
-              interval = setInterval(() => {
-                xAngle = (xAngle + 0.25) % 360;
-                this.geoProjection.rotate([xAngle, 0]);
-                globe.attr('d', geoPath);
+              this.interval = setInterval(() => {
+                this.xAngle = (this.xAngle + 0.25) % 360;
+                this.geoProjection.rotate([this.xAngle, 0]);
+                globe.attr('d', path);
               }, 100);
               globe.classed('clicked', false);
               text.text(null);
             }
           })
-          .attr('d', geoPath);
+          .attr('d', path);
 
-      interval = setInterval(() => {
-        xAngle = (xAngle + 0.25) % 360;
-        this.geoProjection.rotate([xAngle, 0]);
-        globe.attr('d', geoPath);
+      this.interval = setInterval(() => {
+        this.xAngle = (this.xAngle + 0.25) % 360;
+        this.geoProjection.rotate([this.xAngle, 0]);
+        globe.attr('d', path);
       }, 100);
     });
   }
 
   @HostListener('window:resize', ['$event'])
   onResize() {
-    this.height = this.el.nativeElement.offsetHeight;
-    this.width = this.el.nativeElement.offsetWidth;
+    this.mapChart.selectAll('*').remove();
+    clearInterval(this.interval);
+    this.subscribe?.unsubscribe();
+    this.initializeGlobe();
   }
 
   private static findColor(id: string): string {
-    return d3.schemeCategory10[(id.charCodeAt(0) - 65) % 10];
+    return schemeCategory10[(id.charCodeAt(0) - 65) % 10];
   }
 }
